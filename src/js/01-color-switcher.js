deff3fe1b80a0f68164197f1f3e43800f820fed8
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-16 18:53:17 +0300

    Create js-code for js-hw-09(ver.1)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
-console.log('Hello');
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('button');
+const dataAttrStart = "data-start";
+const dataAttrStop = "data-stop";
+
+let intervalId = null;
+
+const bodyChangeColor = addEventListener('click', onClickButton);
+
+function onClickButton(evt) {
+	const target = evt.target;
+	console.log(target);
+	if (target.hasAttribute(dataAttrStart)) {
+		target.disabled = true;
+		intervalId = setInterval(() => {
+			const randomColor = getRandomHexColor();
+			bodyEl.style.backgroundColor = randomColor;
+		}, 1000);
+	}
+	if (target.hasAttribute(dataAttrStop)) {
+		clearInterval(intervalId);
+		buttonEl.disabled = false;
+	}
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
