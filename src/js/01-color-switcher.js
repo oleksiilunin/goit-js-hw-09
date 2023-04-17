54e84a533d358fd12fbd864b2a09fba62342e880
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-17 17:51:31 +0300

    Create js-code for js-hw-09(version 2)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,53 @@
+// const bodyEl = document.querySelector('body');
+// const buttonEl = document.querySelector('button');
+// const dataAttrStart = "data-start";
+// const dataAttrStop = "data-stop";
+
+// let intervalId = null;
+
+// const bodyChangeColor = addEventListener('click', onClickButton);
+
+// function onClickButton(evt) {
+// 	const target = evt.target;
+// 	console.log(target);
+// 	if (target.hasAttribute(dataAttrStart)) {
+// 		target.disabled = true;
+// 		intervalId = setInterval(() => {
+// 			const randomColor = getRandomHexColor();
+// 			bodyEl.style.backgroundColor = randomColor;
+// 		}, 1000);
+// 	}
+// 	if (target.hasAttribute(dataAttrStop)) {
+// 		clearInterval(intervalId);
+// 		buttonEl.disabled = false;
+// 	}
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+// }
+
 const bodyEl = document.querySelector('body');
-const buttonEl = document.querySelector('button');
-const dataAttrStart = "data-start";
-const dataAttrStop = "data-stop";
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
 
 let intervalId = null;
 
-const bodyChangeColor = addEventListener('click', onClickButton);
+const buttonStartChangeColor = addEventListener('click', onClickButtonStart)
+const buttonStopChangeColor = addEventListener('click', onClickButtonStop)
 
-function onClickButton(evt) {
-	const target = evt.target;
-	console.log(target);
-	if (target.hasAttribute(dataAttrStart)) {
-		target.disabled = true;
-		intervalId = setInterval(() => {
-			const randomColor = getRandomHexColor();
-			bodyEl.style.backgroundColor = randomColor;
-		}, 1000);
-	}
-	if (target.hasAttribute(dataAttrStop)) {
+function onClickButtonStart() {
+	buttonStart = true;
+	intervalId = setInterval(() => {
+		const randomColor = getRandomHexColor();
+		bodyEl.style.backgroundColor = randomColor;
+	}, 1000);
+}	
+
+function onClickButtonStop() {
 		clearInterval(intervalId);
-		buttonEl.disabled = false;
+		buttonStart.disabled = false;
 	}
-}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
