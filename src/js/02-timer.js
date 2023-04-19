9d1d6894c67a83e0a98707cd534eae50de7221e2
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-04-20 00:59:46 +0300

    Finish create js-code in 02-timer.js for js-hw-09(task 02)

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,49 +1,85 @@
 // Описан в документации
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 
+const inputDateEl = document.querySelector('#datetime-picker');
 const buttonStartEl = document.querySelector('[data-start]');
 const daysSpanEl = document.querySelector('[data-days]');
 const hoursSpanEl = document.querySelector('[data-hours]');
 const minutesSpanEl = document.querySelector('[data-minutes]');
 const secondsSpanEl = document.querySelector('[data-seconds]');
 
+let timerId = null;
+
 buttonStartEl.disabled = true;
 
 const options = {
-	enableTime: true,
-	time_24hr: true,
-	defaultDate: new Date(),
-	minuteIncrement: 1,
-	onClose(selectedDates) {
-	
-		const currentDate = new Date();
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDateUponSelection = new Date();
+    const selectedDate = selectedDates[0];
 
-		differenceInTime = selectedDates[0] - currentDate;
-
-		if (differenceInTime <= 0) {
-
-			window.alert("Please choose a date in the future")
-			return;
-		}
-
-		const { days, hours, minutes, seconds } = convertMs(differenceInTime);
-
-		daysSpanEl.textContent = days.toString().padStart(2, "0");
-		hoursSpanEl.textContent = hours.toString().padStart(2, "0");
-		minutesSpanEl.textContent = minutes.toString().padStart(2, "0");
-		secondsSpanEl.textContent = seconds.toString().padStart(2, "0");
-
-		console.log(daysSpanEl);
-		console.log(hoursSpanEl);
-		console.log(minutesSpanEl);
-		console.log(secondsSpanEl);
-		
-	},
+    if (selectedDate <= currentDateUponSelection) {
+      window.alert('Please choose a date in the future');
+      buttonStartEl.disabled = true;
+      return;
+    }
+    buttonStartEl.disabled = false;
+  },
 };
 
-const fp = flatpickr('#datetime-picker', options);
+const fp = flatpickr(inputDateEl, options);
+
+buttonStartEl.addEventListener('click', onClickButtonStart);
+
+function onClickButtonStart() {
+  buttonStartEl.disabled = true;
+
+  const selectedDateInInput = new Date(inputDateEl.value);
+  const currentDateUponClickButton = new Date();
+  let differenceInTime = selectedDateInInput - currentDateUponClickButton;
+
+  if (differenceInTime <= 0) {
+    window.alert('Please choose a date in the future');
+    buttonStartEl.disabled = true;
+    return;
+  }
+
+  const { days, hours, minutes, seconds } = convertMs(differenceInTime);
+
+  daysSpanEl.textContent = addLeadingZero(days);
+  hoursSpanEl.textContent = addLeadingZero(hours);
+  minutesSpanEl.textContent = addLeadingZero(minutes);
+  secondsSpanEl.textContent = addLeadingZero(seconds);
+
+  timerId = setInterval(onStart, 1000);
+  let countTime = Math.floor(differenceInTime / 1000) * 1000;
+
+  function onStart() {
+    countTime -= 1000;
+
+    const { days, hours, minutes, seconds } = convertMs(countTime);
+
+    daysSpanEl.textContent = addLeadingZero(days);
+    hoursSpanEl.textContent = addLeadingZero(hours);
+    minutesSpanEl.textContent = addLeadingZero(minutes);
+    secondsSpanEl.textContent = addLeadingZero(seconds);
+
+    if (!countTime) {
+      clearInterval(timerId);
+      return;
+    }
+  }
+
+  fp.set('readonly', true);
+  // inputDateEl.setAttribute('readonly', 'readonly');
+  inputDateEl.style.opacity = '0.5';
+  inputDateEl.style.pointerEvents = 'none';
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -64,6 +100,6 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
